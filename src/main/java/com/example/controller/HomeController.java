4b366766607e12e18afec161dcf863b22d125dc0
shinyay <shinya.com@gmail.com>
2019-03-09 18:21:07 +0900

    Add Mustache Variables

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 class HomeController {
 	@GetMapping
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("title", "Mustache Application");
 		return "index";
 	}
 }
---
+++ src/main/java/com/example/controller/LayoutAdvice.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class LayoutAdvice {
+
+    @ModelAttribute
+    public void defaultLayout(Model model) {
+        model.addAttribute("title", "Mustache Demo Application");
+    }
+}
