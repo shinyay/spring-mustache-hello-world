1ae44b26223cdef9ad51acfebc803d8839dfb8e1
shinyay <shinya.com@gmail.com>
2019-03-14 21:34:12 +0900

    Add Default Value for Mustache Template with Mustache.Compiler

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,28 @@
 package com.example;
 
+import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public Mustache.Compiler compileMustache(Mustache.TemplateLoader templateLoader, Environment environment) {
+
+		MustacheEnvironmentCollector collector = new MustacheEnvironmentCollector();
+		collector.setEnvironment(environment);
+
+		return Mustache.compiler()
+				.defaultValue("DEFAULT VALUE")
+				.withLoader(templateLoader)
+				.withCollector(collector);
 	}
 }
 
