26532cf0aba38f9510a6fdcadc9c1e1b687ec06d
shinyay <shinya.com@gmail.com>
2019-03-14 20:36:58 +0900

    Add list type attribute which is used for iteratation description on Mustache template

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -5,12 +5,54 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 @RequestMapping("/")
 class HomeController {
 	@GetMapping
-	public String home(Model model) {
+	public String index(Model model) {
 		model.addAttribute("title", "Mustache Application");
+
+		List itemList = IntStream.range(0,9).mapToObj(i -> getItem(i)).collect(Collectors.toList());
+		model.addAttribute("itemList", itemList);
+
 		return "index";
+	}
+	private Item getItem(int i){
+		return new Item(String.valueOf(i),"Item"+i, "Sample Description "+i, 100+i);
+	}
+
+	private class Item{
+		private String code;
+
+		public String getCode() {
+			return code;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public int getNumber() {
+			return number;
+		}
+
+		private String name;
+		private String description;
+		private int number;
+
+		public Item(String code, String name, String description, int number) {
+			this.code = code;
+			this.name = name;
+			this.description = description;
+			this.number = number;
+		}
 	}
 }
