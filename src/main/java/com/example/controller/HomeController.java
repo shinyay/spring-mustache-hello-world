100fc1fd841a6a96ffa94128527c74caaa5bf826
shinyay <shinya.com@gmail.com>
2019-03-16 22:03:23 +0900

    Add model.addAttribute for value list

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -15,6 +17,8 @@ class HomeController {
 	@GetMapping
 	public String index(Model model) {
 		model.addAttribute("title", "Mustache Application");
+
+		model.addAttribute("tags", new ArrayList<>(Arrays.asList("spring", "springboot", "mustache")));
 
 		List itemList = IntStream.range(0,9).mapToObj(i -> getItem(i)).collect(Collectors.toList());
 		model.addAttribute("itemList", itemList);
